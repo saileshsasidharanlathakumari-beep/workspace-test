31301bdff0473ca2244e3cea53f204d82e5fb5b1
Neuraverse Workspace <workspace-bot@neura-robotics.com>
2026-09-17 09:39:22 +0000

    resolve: keep workspace: ch-testcustomui-1a0aeb2de15 0.0.1 — 2026-09-17T09:39:22.395Z

--- testlocal-hmi copy/src/main.tsx
+++ testlocal-hmi copy/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react ssss";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
@@ -9,4 +9,3 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </StrictMode>,
 );
-‚
